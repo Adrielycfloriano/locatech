4423bd4c58c861c117e5c942266d5d1b9725bd7d
Adriely Floriano <56102850+Adrielycfloriano@users.noreply.github.com>
2025-06-22 15:00:54 -0300

    Add validation annotations to AluguelRequestDTO fields.

--- src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelRequestDTO.java
+++ src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelRequestDTO.java
@@ -1,10 +1,15 @@
 package br.com.fiap.locatech.locatech.dtos;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record AluguelRequestDTO(
-        long pessoaId,
-        long veiculoId,
+
+        @NotNull(message = "O id da pessoa não pode ser nulo")
+        Long pessoaId,
+        @NotNull(message = "O id do véiculo não pode ser nulo")
+        Long veiculoId,
         LocalDate dataInicio,
         LocalDate dataFim
 ) {
